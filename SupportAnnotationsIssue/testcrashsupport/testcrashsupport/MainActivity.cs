4b94cd079064616ef223b7e4727260b8abd2190c
Timothé Larivière <timothe.lariviere@outlook.com>
2019-06-10 16:45:07 +0200

    Xamarin.Android.Support.Annotations.targets bug

---
+++ SupportAnnotationsIssue/testcrashsupport/testcrashsupport/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace testcrashsupport
+{
+    [Activity(Label = "Crash", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
+
